bbfba1face6be3aa976735e9a20a5b25ceac15c4
changwoo <speed5312@naver.com>
2024-09-26 11:42:33 +0900

    출력 기능 배움

--- src/day01/ExClass01.java
+++ src/day01/ExClass01.java
@@ -1,5 +1,10 @@
 package day01;
 
 public class ExClass01 {
-
+	public static void main(String[] args) {
+		//syso + ctrl + space 자동완성
+		//sysout 는 괄호의 내용을 출력하는 용도
+		System.out.println(100);
+		System.out.println("안녕 클레오파트라 세상에서 제일 가는 포테이토칩");
+	}
 }
