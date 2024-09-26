6fb29f282c68a5f2a5e3e29ef0a3b5b7ff36efaf
changwoo <speed5312@naver.com>
2024-09-26 13:09:01 +0900

    엔터 없는 출력

--- src/day01/ExClass01.java
+++ src/day01/ExClass01.java
@@ -1,10 +1,16 @@
 package day01;
-
+//class 이름 규칙 : 첫 문자 대문자, 합성어 있으면 첫 문자 대문자
 public class ExClass01 {
+	//main은 프로그램의 시작점
 	public static void main(String[] args) {
+	//한 줄 주석
+		/*문단 주석*/
 		//syso + ctrl + space 자동완성
 		//sysout 는 괄호의 내용을 출력하는 용도
 		System.out.println(100);
 		System.out.println("안녕 클레오파트라 세상에서 제일 가는 포테이토칩");
+		System.out.print("1.안녕");
+		System.out.print("2.안녕");
+		System.out.print("3.안녕");
 	}
 }
